a49bfc443ae4fbd483f8497d8299c8dd0eb31361
Victoria Blackburn <victoriablackburn@Victorias-MacBook-Air.local>
2020-07-20 21:32:34 -0700

    added files

---
+++ assets/script.js
@@ -0,0 +1,159 @@
+// Assignment Code
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+function getPasswordOptions(){
+  var pwLength=parseInt(prompt("Enter a number between 8 and 128"));
+   
+
+  var upperCase= confirm("Do you want to include upper case letters?");
+    console.log(upperCase);
+
+  var lowerCase= confirm("Do you want to include lower case letters?");
+    console.log(lowerCase);
+
+
+  var userSpecial=confirm("Do you want to include special characters?");
+    console.log(userSpecial);
+
+  var passwordOptions ={
+    pwLength:pwLength,
+    upperCase:upperCase,
+    lowerCase:lowerCase,
+    userSpecial:userSpecial
+  }
+return passwordOptions
+}
+// Write password to the #password input
+function getRandom(arr){
+  var randomIndex=Math.floor(Math.random() * 9) // arr.length);
+  var randomEl=arr[randomIndex];
+  return randomEl;
+}
+
+function writePassword() {
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+function generatePassword(){
+  var options = getPasswordOptions();
+  var result=[]
+  var possibleCharacters=[]
+  var guaranteeCharacters=[]
+  if (options.userCase){
+    possibleCharacters=possibleCharacters.concat(specialCharacters)
+    guaranteeCharacters.push(getRandom(specialCharacters))
+  }
+  if (options.upperCase){
+  possibleCharacters.concat(upperCasedCharacters)
+  guaranteeCharacters.push(getRandom(upperCasedCharacters))
+  }
+  if (options.lowerCase){
+    possibleCharacters.concat(lowerCasedCharacters)
+    guaranteeCharacters.push(getRandom(lowerCasedCharacters))
+    }
+
+  if (options.numericCharacters){
+    possibleCharacters.concat(numericCharacters)
+    guaranteeCharacters.push(getRandom(numericCharacters))
+    }
+  
+  for (var index = 0; index < options.pwLength; index++) {
+    var possibleCharacters=getRandom(possibleCharacters)
+    result.push(possibleCharacters)
+  }
+  for (var index = 0; index < guaranteeCharacters.length; index++) {
+    result[index]=guaranteeCharacters[index]
+    
+  }  
+  return result.join("")
+}
+
+var generateBtn=document.querySelector("#generate")
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
